bda90671e79cc49df56f0414fe7f45fab8e92fc8
Saidkodirov <stolibjon123@gmail.com>
2024-05-03 23:21:52 +0500

    Add requirements.txt and .Env

--- core/settings.py
+++ core/settings.py
@@ -14,18 +14,17 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+from decouple import config
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c&kmhtee9#s19*nu!uxr&)d9)jp2b-50ona_wkm+#hb7f!%^$m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","127.0.0.1:8000",".pythonanywhere.com"]
 
 
 # Application definition
